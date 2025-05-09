832e50c691b770bfab9e939fb22ff2fc01b66b38
Franksabu <franksabushimike@gmail.com>
2025-05-09 08:51:59 +0200

    first CI/CD and github+dockerhub

---
+++ big_accessdata/asgi.py
@@ -0,0 +1,6 @@
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'big_accessdata.settings')
+
+application = get_asgi_application()
---
+++ products/views.py
@@ -0,0 +1,32 @@
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product
+from .forms import ProductForm
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products})
+
+
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'products/product_form.html', {'form': form})
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+
+    
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'products/product_confirm_delete.html', {'product': product})
